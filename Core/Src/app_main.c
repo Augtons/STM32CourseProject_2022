7c6327a2b3e4274a9a5722012fb8fc7ab5ca49ac
Augtons <79037928+Augtons@users.noreply.github.com>
2022-10-05 23:38:52 +0800

    把TAB制表符替换为空格

--- Core/Src/app_main.c
+++ Core/Src/app_main.c
@@ -83,7 +83,7 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
-	if (htim == &htim1) {
-		smg_flush(&smg);
-	}
+  if (htim == &htim1) {
+    smg_flush(&smg);
+  }
 }
